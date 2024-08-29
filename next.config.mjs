7e33dde7cd8100602ef3368a248bb955a9fa3ad9
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-08-30 03:58:31 +0530

    change

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
     async headers() {
         return [
           {
+            // Matching all API routes
+            source: "/api/:path*",
+            headers: [
+              { key: "Access-Control-Allow-Credentials", value: "true" },
+              { key: "Access-Control-Allow-Origin", value: "https://4148172018.canvas-app.dscvr.one" }, // Replace with your actual origin(s)
+              { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+              {
+                key: "Access-Control-Allow-Headers",
+                value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              },
+            ],
+          },
+
+          {
             source: "/(.*)",
             headers: [
               {
