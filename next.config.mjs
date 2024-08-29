5c250a38015c1448608bdf852425b6147c2ad3e7
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-08-30 04:23:40 +0530

    change

--- next.config.mjs
+++ next.config.mjs
@@ -22,7 +22,7 @@ const nextConfig = {
               {
                 key: "Content-Security-Policy",
                 value:
-                  "connect-src 'self' https://dancing-faun-bf5242.netlify.app/api/v1/users/signin https://dancing-faun-bf5242.netlify.app https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://api.devnet.solana.com wss://api.devnet.solana.com/; style-src 'self' https://fonts.googleapis.com ; style-src 'self' ; img src 'self' blob: data: https://miro.medium.com/v2/resize:fit:1024/1*yBt65HhmARbqZDDJ1McFDg.png;",
+                  "connect-src 'self' https://dancing-faun-bf5242.netlify.app https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://api.devnet.solana.com wss://api.devnet.solana.com/; style-src 'self' https://fonts.googleapis.com ; style-src 'self' ; img src 'self' blob: data: https://miro.medium.com/v2/resize:fit:1024/1*yBt65HhmARbqZDDJ1McFDg.png;",
               },
             ],
           },
