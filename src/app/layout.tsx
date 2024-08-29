4acf2e8ce67ac1a16cf405184dce77bc2aaaee2a
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-08-30 03:27:21 +0530

    change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,12 +22,12 @@ export const metadata: Metadata = {
     title: "DAO",
     description: "Create DAO Voting - Powered by Solana",
     type: "website",
-    url: "https://dao-frontend-beta.vercel.app/",
-    images: "https://news.miami.edu/_assets/images-stories/2023/02/dao-web3-hero-940x529.jpg"
+    url: "https://dancing-faun-bf5242.netlify.app",
+    images: "https://news.miami.edu/_assets/images-stories/2023/02/dao-web3-hero-940x529.jpg",
   },
   other: {
-    'dscvr:canvas:version': "vNext",
-  }
+    "dscvr:canvas:version": "vNext",
+  },
 };
 
 
